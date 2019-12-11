7db399e84861e2baa8f04a565ebe3087e322e69b
Alekky09 <Alekky09@users.noreply.github.com>
2019-12-11 01:56:16 +0000

    automated commit by check50 [check50=True]

--- bleep.py
+++ bleep.py
@@ -8,24 +8,22 @@ def main():
         exit("Usage: python bleep.py dictionary")
 
     dictionary = open(argv[1], "r")
-
     file = {line.strip() for line in dictionary}
-    print(file)
-
     message = get_string("What message would you like to censor?\n")
     message_list = message.split()
 
     for word in message_list:
         case = word.lower()
 
-
         if case in file:
             for char in word:
                 print("*", end = "")
+
         else:
             print(word, end = "")
 
         print(" ", end = "")
+
     print("")
 
 if __name__ == "__main__":
